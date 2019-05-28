79e9ad03d102cb644137fce1f3a7f01586ca051a
Dima <ignat@lampa.by>
2019-05-28 11:29:53 +0300

    таймер

--- less8/js/script.js
+++ less8/js/script.js
@@ -32,5 +32,61 @@ window.addEventListener('DOMContentLoaded', function(){
                 }
             }
         }
-    })
-})
+    });
+
+    //timer
+
+    let deadline = '2019-05-29';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()), //дата окончания - тек дата
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/1000/60/60));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds,
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime),
+            h = t.hours,
+            m = t.minutes,
+            s = t.seconds;
+            if(t.hours <= 9) {
+                h = '0' + t.hours;
+                };
+            hours.textContent = h;
+            if (t.minutes <= 9) {
+                m = '0' + t.minutes;
+                };
+            minutes.textContent = m;    
+            if (t.seconds <= 9) {
+                s = '0' + t.seconds;
+                };    
+            seconds.textContent = s;    
+
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+
+        }    
+    }
+
+    setClock('timer', deadline);
+});
